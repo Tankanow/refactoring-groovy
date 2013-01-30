e0aaf7698d8d2c4f3b334c9bab71b26be5077a32
Adam Tankanow <Adam.Tankanow@gmail.com>
2013-01-29 21:19:24 -0500

    Move Method Patter:
    	- generally, methods should act on data within their class: in this example, amountFor acts solely
    	on data in Rental and not on data in Customer; therefore, move amountFor to the Rental class
    
    	- we rename the method to the more meaningful name, getCharge, and simply delegate from the
    	old amountFor to the new getCharge

--- aFirstExample/src/main/groovy/Rental.groovy
+++ aFirstExample/src/main/groovy/Rental.groovy
@@ -15,4 +15,31 @@ class Rental {
 		this.movie = movie
 		this.daysRented = daysRented
 	}
+
+
+	/**
+	 * Get the change for this rental
+	 **/
+	double getCharge(){
+
+		double result = 0
+		//determine amounts for each line
+		switch (movie.getPriceCode()) {
+			case Movie.REGULAR: 
+				result += 2
+				if (daysRented > 2)
+					result += (daysRented - 2) * 1.5
+				break
+			case Movie.NEW_RELEASE:
+				result += daysRented * 3
+				break
+			case Movie.CHILDRENS:
+				result += 1.5
+				if (daysRented > 3)
+					result += (daysRented - 3) * 1.5 
+				break
+		}
+
+		return result
+	}
 }
