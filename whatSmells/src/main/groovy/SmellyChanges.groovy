0c81917ae8a8818e4fc6592dcb57405f83f8a50a
Adam Tankanow <Adam.Tankanow@gmail.com>
2013-03-17 21:32:22 -0400

    added Divergent Change and Shotgun Surgery smells in one class called SmellyChanges

---
+++ whatSmells/src/main/groovy/SmellyChanges.groovy
@@ -0,0 +1,14 @@
+class SmellyChanges {
+
+	private class DivergentChange {
+
+		// one class changes for lots of reasons
+	}
+
+	private class ShotgunSurgery {
+
+		// one change affects lots of classes
+
+	}
+	
+}
